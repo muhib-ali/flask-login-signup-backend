338e3d5616787447c40c4f2e1336304774c0f9bc
muhib-ali <pc15981.muhib@gmail.com>
2025-06-12 21:33:04 +0500

    Update __init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -9,7 +9,7 @@ DB_NAME="blogdb"
 def create_app():
     app=Flask(__name__)
     app.config['SECRET_KEY']="helloworld"
-    app.config['SQLALCHEMY_DATABASE_URI']=f'postgresql://postgres:1234@localhost:5432/{DB_NAME}'
+    app.config['SQLALCHEMY_DATABASE_URI']=f'postgresql://postgres:<password>@localhost:<port>/{DB_NAME}'
     db.init_app(app)
 
     from .views import views
